ce01f28de3a62d916da41d092508324c11c798f3
Dominic Magnifico <dominicmagnifico@gmail.com>
2019-10-24 15:55:14 -0600

    Move Button styles to their own exported folder. Adds the Head component.

---
+++ src/components/Global/Head.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Helmet from 'react-helmet'
+
+const Head = () => (
+  <Helmet>
+    <link href="https://fonts.googleapis.com/css?family=Quattrocento+Sans:400,400i,700|Work+Sans:600&display=swap" rel="stylesheet" />
+  </Helmet>
+)
+
+export default Head
